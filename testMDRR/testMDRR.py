8a8f00d44e647b7a5df1322c2a6eaf375efb53f4
damjanmk <damjanmk@hotmail.com>
2018-04-25 15:39:33 +0100

    Adding some code to test the mdrr_small database. This file is for testing methods that connect to the MongoDB database.

--- testMDRR/testMDRR.py
+++ testMDRR/testMDRR.py
@@ -8,9 +8,18 @@ import bson.json_util
 client = MongoClient()      
 db = client.mdrr_1
 
-
-
-
+ligands = db.ligandsSmall.find()
+receptors = db.receptorsSmall.find()
+ligand_ids = []
+receptor_ids = []
+for l in ligands:
+    ligand_ids.append(str(l["structure_id"]))
+for r in receptors:
+    receptor_ids.append(str(r["structure_id"]))
+print ligand_ids
+print receptor_ids
+# results = db.results.find({"ligand": {"$in": ligand_ids}, "receptor": {"$in": receptor_ids} })
+# print len(results)
 #test_pybel insert methods
 # i = Inserter(db)
 # result = i.insert_receptor("/home/damjan/Documents/Docking/result data/results/rec.pdbqt", "ribokinase", "TV")
@@ -40,12 +49,12 @@ db = client.mdrr_1
 #     result_id = i.insert_result(receptor, "ribokinase", "TV", ligand, "(S)-(+)-Mandelic acid", result, config_file)
 
 #test_pybel selector methods
-s = Selector(db)
+# s = Selector(db)
 # results = [bson.ObjectId("59522cc4c831de0e7c524636"), "59522cc4c831de0e7c524639", "5952217cc831de0e7c52461a", "59998c29c831de10b17eed58", "592f2cfac831de25fe46f1d7"]
-results = ["5a5cc8e8c831de11a5d80a79", "5a5cc8e8c831de11a5d80aca", "5a5cc8e8c831de11a5d80a9a", "5a5cc8e8c831de11a5d80a91"]
-cursor = s.select_results_grouped_format(results)
-print "lala"
-print bson.json_util.dumps(cursor)
+# results = ["5a5cc8e8c831de11a5d80a79", "5a5cc8e8c831de11a5d80aca", "5a5cc8e8c831de11a5d80a9a", "5a5cc8e8c831de11a5d80a91"]
+# cursor = s.select_results_grouped_format(results)
+# print "lala"
+# print bson.json_util.dumps(cursor)
 # similar_structure_ids = ["1f64a6e0a4f0a8588f40ad3dab7eafde", "15e7679635b2001077b825d9b6311268"]
 # cursor = s.select_results_where_receptor_structure_ids(similar_structure_ids)
 # print bson.json_util.dumps(cursor)
